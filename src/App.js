fc6b01a04d84a76c60118a965e39320af7ef21ac
Beau Sherman <brsherman88@gmail.com>
2021-04-07 23:25:00 -0700

    Meet all requirements

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import './App.css';
 
 function App() {
   const [markdown, setMarkdown] = useState(`
-  # Hello World!
+  # Welcome to my Markdown Previewer!
 
-  ## This is a subheading
+  ## This is a subheading...
 
-  _This is italic_
+  [This is a project for FreeCodeCamp](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer)
+
+  Here's some code: \`<div></div>\`
+
+  Here's some more code:
+
+  \`\`\`javascript
+  const greeting = 'Hello from markdown!';
+  alert(greeting);
+  \`\`\`
+
+  - This is a list item
+  
+  1. This is a numbered list item
+
+  > This is a block quote
+
+  ![logo](./logo512.png "Logo")
+
+  And last but not least, here is some __bolded text__
 `);
 
   const [html, setHtml] = useState(() => marked(markdown));
