5c84c623daa47afb2b3b0749c254658d686ef8a3
Beau Sherman <brsherman88@gmail.com>
2021-04-07 23:08:45 -0700

    Add basic functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import {useState, useEffect} from 'react';
-
-import {marked} from 'marked';
-
+import {useState, useEffect, useRef} from 'react';
+import marked from 'marked';
 import './App.css';
 
 function App() {
+  const [markdown, setMarkdown] = useState(`
+  # Hello World!
+
+  ## This is a subheading
+
+  _This is italic_
+`);
+
+  const [html, setHtml] = useState(() => marked(markdown));
+
+  const preview = useRef();
+
+  useEffect(() => {
+    setHtml(marked(markdown));
+  }, [markdown]);
+
+  useEffect(() => {
+    preview.current.innerHTML = html;
+  }, [html]);
+
+  const handleChange = evt => {
+    setMarkdown(markdown => evt.target.value);
+  };
+
   return (
     <div className="App">
       <h1>Markdown Previewer</h1>
-      <textarea name="editor" id="editor" cols="30" rows="10">
-        # Enter markdown here ## See the output in the previewer
+      <textarea
+        name="editor"
+        id="editor"
+        cols="30"
+        rows="10"
+        onChange={handleChange}
+      >
+        {markdown}
       </textarea>
-      <div id="preview"></div>
+      <div id="preview" ref={preview}></div>
     </div>
   );
 }
