d2a40d24d290056457d2d9acff7f50d04c56d13a
Beau Sherman <brsherman88@gmail.com>
2021-04-07 22:42:20 -0700

    Run create-react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+
+import {marked} from 'marked';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Markdown Previewer</h1>
+      <textarea name="editor" id="editor" cols="30" rows="10">
+        # Enter markdown here ## See the output in the previewer
+      </textarea>
+      <div id="preview"></div>
     </div>
   );
 }
